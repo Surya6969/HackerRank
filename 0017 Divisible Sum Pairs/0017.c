17fcbcdd225e764d9b3379679a3978022e04f5cb
Martin Kuna <martinkuna@users.noreply.github.com>
2018-03-07 16:57:17 -0700

    Added C Lang

--- 0017 Divisible Sum Pairs/0017.c
+++ 0017 Divisible Sum Pairs/0017.c
@@ -1,10 +1,5 @@
-#include <math.h>
 #include <stdio.h>
-#include <string.h>
 #include <stdlib.h>
-#include <assert.h>
-#include <limits.h>
-#include <stdbool.h>
 
 int divisibleSumPairs(int n, int k, int ar_size, int* ar) {
     // Complete this function
